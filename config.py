9673734577da22f7277b0a11a3b7d1fce3933999
Your Name <sinkondeluvmo11@gmail.com>
2024-01-04 22:07:00 +0200

    MySQL Connection Tshhoot

--- config.py
+++ config.py
@@ -6,12 +6,6 @@ load_dotenv()
 class Config:
     DEBUG = True
     SECRET_KEY = os.getenv('SECRET_KEY')
-    # MySQL Configuration
     SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-
-# Export SECRET_KEY to .env file
-if __name__ == "__main__":
-    with open(".env", "a") as f:
-        f.write(f"SECRET_KEY={Config.SECRET_KEY}\n")
 
